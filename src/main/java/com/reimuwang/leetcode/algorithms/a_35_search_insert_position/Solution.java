ce8133ab25c41b6a792053515b35813c439d05dc
yl.wyk <yl.wyk@alibaba-inc.com>
2020-11-03 14:52:56 +0800

    35

--- src/main/java/com/reimuwang/leetcode/algorithms/a_35_search_insert_position/Solution.java
+++ src/main/java/com/reimuwang/leetcode/algorithms/a_35_search_insert_position/Solution.java
@@ -3,17 +3,18 @@ package com.reimuwang.leetcode.algorithms.a_35_search_insert_position;
 public class Solution {
 
   public int searchInsert(int[] nums, int target) {
-    int n = nums.length;
-    int left = 0, right = n - 1, ans = n;
+    int left = 0;
+    int right = nums.length - 1;
+    int result = nums.length;
     while (left <= right) {
       int mid = left + (right - left) / 2;
-      if (target <= nums[mid]) {
-        ans = mid;
-        right = mid - 1;
-      } else {
+      if (target > nums[mid]) {
         left = mid + 1;
+        continue;
       }
+      result = mid;
+      right = mid - 1;
     }
-    return ans;
+    return result;
   }
 }
